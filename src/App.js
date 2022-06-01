62497800ab8e7f0a09c27939ae93eec94f8d9a85
jaouad <Jadsarouti@outlook.com>
2022-06-01 14:48:16 -0400

    Creating a component | looping through data.JS

--- src/App.js
+++ src/App.js
@@ -6,31 +6,17 @@ import australia from "./images/australiajpg.jpg"
 import norway from "./images/norway.jpg"
 import data from "./data/data"
 function App() {
+  const renderContent = data.map(
+    content => <MainContent image={content.image}
+      location={content.location}
+      descPicture={content.descPicture}
+      date={content.date}
+      description={content.description} />)
 
   return (
     <div className="container">
       <Header />
-      <MainContent
-        image={japan}
-        location={data[0].location}
-        descPicture={data[0].desPicture}
-        date={data[0].date}
-        description={data[0].description}
-      />
-      <MainContent
-        image={australia}
-        location={data[1].location}
-        descPicture={data[1].desPicture}
-        date={data[1].date}
-        description={data[1].description}
-      />
-      <MainContent
-        image={norway}
-        location={data[2].location}
-        descPicture={data[2].desPicture}
-        date={data[2].date}
-        description={data[2].description}
-      />
+      {renderContent}
     </div>
   );
 }
