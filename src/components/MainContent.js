c79125dbe492d53cf97dd5da25a502092a3ce6b6
jaouad <Jadsarouti@outlook.com>
2022-06-01 00:37:52 -0400

    swtiching data to its prope file JS

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,17 +4,17 @@ import japan from "../images/japan.jpg"
 const MainContent = () => {
     return (
         <section className="main-content">
-            <div>
-                <img src={japan} alt="Japan" />
+            <div >
+                <img className="image-content" src={japan} alt="Japan" />
             </div>
             <div className="main-content-description">
-                <div>
-                    <h4>Japan</h4>
-                    <p>view on Google</p>
+                <div className="description">
+                    <h4 className="title-country">JAPAN</h4>
+                    <p className="title-google">view on Google</p>
                 </div>
-                <h4>Mount Fuji</h4>
-                <p>Date</p>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).
+                <h4 className="pic-desc">Mount Fuji</h4>
+                <p className="date">Date</p>
+                <p className="content-paragraph">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).
                     Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists</p>
 
             </div>
